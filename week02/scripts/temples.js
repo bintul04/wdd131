46282064810c075e2632235accf7f7ca94cfd788
bintul04 <lukulayb47@gmail.com>
2025-03-21 15:03:36 +0000

    Temple Album

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -14,14 +14,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// Hamburger Menu Toggle with GIF Animation
+// Hamburger Menu Toggle
 const menuButton = document.getElementById("menu-button");
 const navMenu = document.querySelector(".nav-menu");
-const menuGif = document.getElementById("menu-gif");
 
 menuButton.addEventListener("click", () => {
     navMenu.classList.toggle("open");
-    menuGif.src = navMenu.classList.contains("open")
-        ? "images/hamburger-close.gif"
-        : "images/hamburger-menu.gif";
+    menuButton.textContent = navMenu.classList.contains("open") ? "✖" : "☰";
 });
